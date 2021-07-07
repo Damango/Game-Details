01be26e99915d4340a4764358b0f4f0c13805110
Justin Kessler <redcoderhd1@gmail.com>
2021-07-07 15:33:37 -0400

    CSS animations

--- src/Components/GameCard/GameCard.js
+++ src/Components/GameCard/GameCard.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 import "./GameCard.css"
 
 
@@ -6,7 +6,18 @@ const GameCard = (props) => {
 
 
     const cardRef = useRef()
+    const rectRef = useRef()
 
+    let movingRects = [];
+    for(let i = 0; i < 4; i++){
+        movingRects.push(Math.floor(Math.random() * 300))
+    }
+    useEffect(() => {
+        
+        for(let i = 0; i < 4; i++){
+            movingRects.push(Math.floor(Math.random() * 300))
+        }
+    }, [])
 
     function makeCardBig(){
         cardRef.current.style.position = 'absolute';
@@ -15,9 +26,47 @@ const GameCard = (props) => {
     }
 
 
+    function moveRect(){
+
+ 
+
+    }
+
+ 
+    
 
 
-    return ( <div className="game-card-container" ref={cardRef} onClick={makeCardBig}>
+
+    return ( <div className="game-card-container" ref={cardRef} >
+        
+        <div className="moving-rects-container">
+            
+
+            
+        </div>
+        <div className="anim-rect" style={{top: movingRects[0]}} ref={rectRef}></div>
+        <div className="anim-rect2" style={{top: movingRects[1]}} ref={rectRef}></div>
+        <div className="anim-rect3"  ref={rectRef}></div>
+        <div className="anim-rect4" ref={rectRef}></div>
+      
+         <div className="game-image center-x" style={{backgroundImage:`url(${props.data.cardImage})` }}></div>
+        <div className="game-title-container">{props.data.title.toUpperCase()}</div>
+        <div className="b ussiness-model"><div className="model-line center-y"></div> {props.data.bussinessModel}</div>
+        <div className="game-tags-container">
+            {props.data.tags.map((tag) => <div className="game-tag">{tag}</div>)}
+        </div>
+        
+    </div> );
+ 
+}
+ 
+export default GameCard;
+
+
+
+/* Other Style 
+
+<div className="game-card-container" ref={cardRef} onClick={makeCardBig}>
          <div className="game-image center-x" style={{backgroundImage:`url(${props.data.cardImage})` }}></div>
         <div className="game-title-container">{props.data.title}</div>
         <div className="bussiness-model"><div className="model-line center-y"></div> {props.data.bussinessModel}</div>
@@ -25,7 +74,12 @@ const GameCard = (props) => {
             {props.data.tags.map((tag) => <div className="game-tag">{tag}</div>)}
         </div>
         
-    </div> );
-}
- 
-export default GameCard;
+    </div>
+
+
+
+
+
+
+
+*/
